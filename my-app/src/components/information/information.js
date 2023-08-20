33e893196cea409a9d881d85c8936f285f4b114c
han Solo <hanolo@yandex.ru>
2023-08-20 22:28:59 +0300

    v.3

--- my-app/src/components/information/information.js
+++ my-app/src/components/information/information.js
@@ -2,8 +2,9 @@ import { useSelector } from 'react-redux';
 import { PLAYER_ACTION, PLAYER_NAME, STATUS } from '../../constants';
 import { selectCurrentPlayer, selectStatus } from '../../selectors';
 import { InformationLayout } from './information-layout';
+import { Component } from 'react';
 
-export const Information = () => {
+/* export const Information = () => {
 	const status = useSelector(selectStatus);
 	const currentPlayer = useSelector(selectCurrentPlayer);
 
@@ -14,4 +15,30 @@ export const Information = () => {
 		status === STATUS.DRAW ? 'Ничья' : `${playerAction}: ${playerName}`;
 
 	return <InformationLayout information={information} />;
-};
+}; */
+
+export class InformationContainer extends Component {
+	constructor(props) {
+		super();
+		this.status = props.status;
+		this.currentPlayer = props.currentPlayer;
+		this.playerAction = props.playerAction;
+		this.playerName = props.playerName;
+		this.information =
+			this.status === STATUS.DRAW
+				? 'Ничья'
+				: `${this.playerAction}: ${this.playerName}`;
+	}
+	render() {
+		return <InformationLayout information={this.information} />;
+	}
+}
+
+const mapStateToProps = ({ status, currentPlayer }) => ({
+	status: status,
+	currentPlayer: currentPlayer,
+	playerAction: PLAYER_ACTION[status],
+	playerName: PLAYER_NAME[currentPlayer],
+});
+
+export const Information = connect(null, mapStateToProps)(InformationContainer);
